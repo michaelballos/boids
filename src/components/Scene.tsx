b93ce518e43cfc1ace67c6a970e3fc149d6465cd
michaelballos <mdb3091207@gmail.com>
2022-04-29 00:05:43 -0500

    Edit Movement

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -8,16 +8,15 @@ import { Physics } from '@react-three/cannon';
 
 function Boid() {
   const ref = useRef<any>([0, 0, 0]);
-
   useFrame(() => {
     const { current } = ref;
     const position = current.position;
     const rotation = current.rotation;
-    position.x = Math.sin(Date.now() * 0.001) * .5;
-    position.y = Math.sin(Date.now() * 0.0001) * 2;
+    position.x = Math.sin(Date.now() * 0.0001) * 5;
     position.z = Math.sin(Date.now() * 0.0005) * 2;
-    rotation.z += .01;
-    rotation.y += .01;
+    position.y = Math.sin(Date.now() * 0.0005) * 2;
+    rotation.y += .02;
+    rotation.x += .02;
   });
 
   return (
